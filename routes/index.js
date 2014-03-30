ebd5564dc3fc3fa6e5e38b693f57e32e80bd57bc
Anthony To <to.anthony93@gmail.com>
2014-03-30 14:44:28 -0500

    Add routing for passport login

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,78 @@
 /*
  * GET home page.
  */
+// exports.index = function(req, res){
+//   res.render('index', {root: './public'});
+// };
 
-exports.index = function(req, res){
-  res.sendfile('index.html', {root: './public'});
+
+// Passport Auth
+// app/routes.js
+module.exports = function(app, passport) {
+
+	app.get('/', function(req, res) {
+		res.render('index.jade'); 
+	});
+	 
+	app.get('/login', function(req, res) {
+
+		// render the page and pass in any flash data if it exists
+		res.render('login.jade', { message: req.flash('loginMessage') }); 
+	});
+
+	// process the login form
+	// app.post('/login', do all our passport stuff here);
+
+	// process the signup form
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/profile', // redirect to the secure profile section
+		failureRedirect : '/signup', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
+	}));
+
+	// process the signup form
+	// app.post('/signup', do all our passport stuff here);
+
+	// =====================================
+	// PROFILE SECTION =====================
+	// =====================================
+	// we will want this protected so you have to be logged in to visit
+	// we will use route middleware to verify this (the isLoggedIn function)
+	app.get('/profile', isLoggedIn, function(req, res) {
+		res.render('profile.jade', {
+			user : req.user // get the user out of session and pass to template
+		});
+	});
+
+	// =====================================
+	// LOGOUT ==============================
+	// =====================================
+	app.get('/logout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
 };
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+	// if user is authenticated in the session, carry on 
+	if (req.isAuthenticated())
+		return next();
+
+	// if they aren't redirect them to the home page
+	res.redirect('/');
+}
+
+exports.users = function(db) {
+	return function(req, res) {
+		var collection = db.get('user');
+		collection.find({}, {}, function(e, docs){
+			res.render('users.jade', {
+				"users" : docs
+			});
+		});
+	};
+};
+
+
